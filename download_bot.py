e9ee462d343b43ad241eba0734ba563e9cbcfe7e
Zidong Li <cestlzd@gmail.com>
2022-10-07 22:01:38 +0800

    run it to start streaming service

--- download_bot.py
+++ download_bot.py
@@ -24,10 +24,10 @@ timeZ_London = pytz.timezone('Africa/Accra')
 dt_London = datetime.now(timeZ_London).replace(tzinfo=None)
 
 def bot():
+    already = []
     while True:
         #after each loop,update timeline
         mentions = api.mentions_timeline() 
-        already = []
         for mention in mentions:
             #only send to those mentions which tweeted after service launched and never sent before
             if (mention.created_at-dt_London).total_seconds()>=0 and mention.id not in already:
